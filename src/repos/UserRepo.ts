c7183571e8f9955b3f5debe36f4ddc392de37704
jdakdaadsa <163021360+Belennav@users.noreply.github.com>
2024-12-14 17:23:26 -0300

    Update UserRepo.ts

--- src/repos/UserRepo.ts
+++ src/repos/UserRepo.ts
@@ -1,85 +1,161 @@
-import { IUser } from '@src/models/User';
-import { getRandomInt } from '@src/util/misc';
-import orm from './MockOrm';
-
-
+import { IUser } from "@src/models/User";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { UserModel } from "./Mongoose";
 // **** Functions **** //
 
 /**
  * Get one user.
  */
-async function getOne(email: string): Promise<IUser | null> {
-  const db = await orm.openDb();
-  for (const user of db.users) {
-    if (user.email === email) {
-      return user;
-    }
-  }
-  return null;
+async function getOne(id: mongoose.Types.ObjectId): Promise<IUser | null> {
+  return new Promise<IUser | null>((resolve, reject) => {
+    UserModel.findOne({ _id: id })
+      .then((user: any) => {
+        resolve(user);
+      })
+      .catch((err: any) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+      });
+  });
 }
 
 /**
  * See if a user with the given id exists.
  */
-async function persists(id: number): Promise<boolean> {
-  const db = await orm.openDb();
-  for (const user of db.users) {
-    if (user.id === id) {
-      return true;
-    }
-  }
-  return false;
+async function persists(id: mongoose.Types.ObjectId): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    UserModel.findOne({ _id: id })
+      .then((user: any) => {
+        if (user) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch((err: any) => {
+        if (err) {
+          reject(err);
+        }
+      });
+  });
 }
 
 /**
  * Get all users.
  */
 async function getAll(): Promise<IUser[]> {
-  const db = await orm.openDb();
-  return db.users;
+  return new Promise<IUser[]>((resolve, reject) => {
+    UserModel.find({})
+      .then((users: any) => {
+        resolve(users);
+      })
+      .catch((err: any) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+      });
+  });
 }
 
 /**
  * Add one user.
  */
 async function add(user: IUser): Promise<void> {
-  const db = await orm.openDb();
-  user.id = getRandomInt();
-  db.users.push(user);
-  return orm.saveDb(db);
+  const saltRounds = 10; // Puedes ajustar el número de rondas según sea necesario
+  return new Promise<void>((resolve, reject) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) reject(err);
+
+      // Hashear la contraseña con el salt generado
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) reject(err);
+
+        // join salt and hash to store in db
+        user.password = hash;
+        UserModel.create(user)
+          .then(() => resolve())
+          .catch((err: any) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+          });
+      });
+    });
+  });
+}
+
+async function login(user: IUser): Promise<string> {
+  // eslint-disable-next-line max-len
+  return UserModel.findOne({ email: user.email }).then(
+    (foundUser: IUser | null) => {
+      if (foundUser) {
+        return bcrypt.compare(user.password, foundUser.password).then((ok) => {
+          if (!ok) {
+            return "";
+          }
+          return foundUser._id.toString();
+        });
+      } else {
+        return "";
+      }
+    }
+  );
 }
 
 /**
  * Update a user.
  */
 async function update(user: IUser): Promise<void> {
-  const db = await orm.openDb();
-  for (let i = 0; i < db.users.length; i++) {
-    if (db.users[i].id === user.id) {
-      const dbUser = db.users[i];
-      db.users[i] = {
-        ...dbUser,
-        name: user.name,
-        email: user.email,
-      };
-      return orm.saveDb(db);
-    }
-  }
+  return new Promise<void>((resolve, reject) => {
+    UserModel.updateOne({ _id: user._id }, user)
+      .then(() => resolve())
+      .catch((err: any) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+  });
 }
 
 /**
  * Delete one user.
  */
-async function delete_(id: number): Promise<void> {
-  const db = await orm.openDb();
-  for (let i = 0; i < db.users.length; i++) {
-    if (db.users[i].id === id) {
-      db.users.splice(i, 1);
-      return orm.saveDb(db);
-    }
-  }
+async function delete_(id: mongoose.Types.ObjectId): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    UserModel.deleteOne({ _id: id })
+      .then(() => resolve())
+      .catch((err: any) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+  });
 }
 
+async function checkIfAdmin(id: mongoose.Types.ObjectId): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    UserModel.findOne({ _id: id })
+      .then((user: any) => {
+        if (user) {
+          resolve(user.isAdmin);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch((err: any) => {
+        if (err) {
+          reject(err);
+        }
+      });
+  });
+}
 
 // **** Export default **** //
 
@@ -88,6 +164,8 @@ export default {
   persists,
   getAll,
   add,
+  login,
   update,
   delete: delete_,
+  checkIfAdmin,
 } as const;
